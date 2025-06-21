774c42703475945379d96d0650ab01b2aa90e2dc
zhangHaoYang611 <haoyangz316@gmail.com>
2025-06-21 13:05:18 +0800

    Final Fix: Rename user table to users to avoid SQL keyword conflict

--- src/main/java/com/example/library/entity/User.java
+++ src/main/java/com/example/library/entity/User.java
@@ -1,10 +1,14 @@
 package com.example.library.entity;
+
+import jakarta.persistence.Table;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
